8b4dd347e62fb1c648a39e2524e295a8f1b2ed2c
Randomphenomena <carito.figuehe@gmail.com>
2024-05-13 16:10:55 +0200

    CNAME added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,21 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import lottie from "astro-integration-lottie";
 
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = 'http://localhost:${SERVER_PORT}';
+const LIVE_URL = "https://randomphenomena.github.io"
+const SCRIPT = process.env.npm_lifecycle.scrpt || "";
 
+const isBuild = SCRIPT.includes("astro build");
+
+let BASE_URL = LOCALHOST_URL;
+if (isBuild){
+  BASE_URL = LIVE_URL
+}  
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://randomphenomena.github.io',
+  server: { port: SERVER_PORT},
+  site: BASE_URL,
   integrations: [
     tailwind(),
     react(),
