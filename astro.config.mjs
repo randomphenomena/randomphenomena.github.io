1255ba07f3faa5d0c0d877e3072f51e06d1d6c25
Randomphenomena <carito.figuehe@gmail.com>
2024-05-13 16:16:41 +0200

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import lottie from "astro-integration-lottie";
 
-const SERVER_PORT = 4321;
-const LOCALHOST_URL = 'http://localhost:${SERVER_PORT}';
-const LIVE_URL = "https://randomphenomena.github.io"
-const SCRIPT = process.env.npm_lifecycle.scrpt || "";
-
-const isBuild = SCRIPT.includes("astro build");
-
-let BASE_URL = LOCALHOST_URL;
-if (isBuild){
-  BASE_URL = LIVE_URL
-}  
+  
 // https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT},
-  site: BASE_URL,
+  
+  site: "https://randomphenomena.github.io",
   integrations: [
     tailwind(),
     react(),
